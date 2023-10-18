8d82b4297072d47d403487f366fa225c432fabdf
hdvgonz <100740343+hdvgonz@users.noreply.github.com>
2023-10-18 09:17:20 -0500

    Update 2_validIsogram.ts

--- Ts_Solutions/2_validIsogram.ts
+++ Ts_Solutions/2_validIsogram.ts
@@ -1,3 +1,24 @@
+/**
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false
+
+Test cases:
+    assert.equal( isIsogram("Dermatoglyphics"), true );
+    assert.equal( isIsogram("isogram"), true );
+    assert.equal( isIsogram("aba"), false, "same chars may not be adjacent" );
+    assert.equal( isIsogram("moOse"), false, "same chars may not be same case" );
+    assert.equal( isIsogram("isIsogram"), false );
+    assert.equal( isIsogram(""), true, "an empty string is a valid isogram" );
+*/
+
+
 export function isIsogram(str: string): boolean{
   
   str = str.toLowerCase();
