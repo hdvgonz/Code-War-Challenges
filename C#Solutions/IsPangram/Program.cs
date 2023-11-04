73e5459c1077810ef5621e967ba5f412f61dad36
danihdv <hdvgonz@gmail.com>
2023-11-03 23:44:58 -0500

    Uploading new c# solved exercises

---
+++ C#Solutions/IsPangram/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsPangram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "The quick brown fox jumps over the lazy dog";
+            
+            IsPangram(message);
+
+            Console.ReadKey();
+        }
+        public static bool IsPangram(string str)
+        {
+            var letters = new List<string>() {"a", "b", "c"};
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str.CompareTo(letters));
+            }
+            return true;
+        }
+    }
+}
+
+/*
+ * A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.*/
---
+++ C#Solutions/convertBoolToString/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace convertBoolToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        public static string BoolToWord(bool word)
+        {
+            string msg = "No";
+
+            if (word)
+            {
+                msg = "Yes";
+                Console.WriteLine(msg);
+                return msg;
+            }
+            Console.WriteLine(msg);
+            return msg;
+        }
+    }
+}
