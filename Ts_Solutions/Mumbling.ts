f5e244a532c344f55deb86751a6ba37507065fd6
hdvgonz <100740343+hdvgonz@users.noreply.github.com>
2023-11-07 23:35:39 -0500

    Create Mumbling.ts

---
+++ Ts_Solutions/Mumbling.ts
@@ -0,0 +1,24 @@
+export function accum(s: string): string {
+ let result = "";
+  let operation:  string = "";
+ 
+  
+  for (let i = 1; i <= s.length; i++){
+    operation += s[i-1].toUpperCase();
+    operation+= s[i-1].toLowerCase().repeat(i-1).concat("-");
+  }
+  
+  result = operation.slice(0, operation.length-1)
+
+  return result
+  }
+/*
+PROBLEM
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+*/
