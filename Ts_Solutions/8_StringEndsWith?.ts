fe10166ea801212967a55384080a47e034ba36ce
hdvgonz <100740343+hdvgonz@users.noreply.github.com>
2023-11-08 23:13:56 -0500

    Create 8_StringEndsWith?.ts

---
+++ Ts_Solutions/8_StringEndsWith?.ts
@@ -0,0 +1,15 @@
+function solution(str: string, ending: string): boolean {
+
+  
+  return str.substring(str.length - ending.length) === ending
+}
+
+/*
+Problem:
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
